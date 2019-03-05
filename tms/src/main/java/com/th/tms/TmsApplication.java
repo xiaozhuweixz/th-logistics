715b1262cd7135a6052dca946c1e1b20e9f2e791
caixiaowei <176047903@qq.com>
2019-03-05 20:32:45 +0800

    tms 跨域请求

--- tms/src/main/java/com/th/tms/TmsApplication.java
+++ tms/src/main/java/com/th/tms/TmsApplication.java
@@ -1,8 +1,12 @@
 package com.th.tms;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,5 +15,21 @@ public class TmsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TmsApplication.class, args);
 	}
-
+	private CorsConfiguration buildConfig() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedOrigin("*");
+		corsConfiguration.addAllowedHeader("*");
+		corsConfiguration.addAllowedMethod("*");
+		return corsConfiguration;
+	}
+	/**
+	 * 跨域过滤器
+	 * @return
+	 */
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", buildConfig()); // 4
+		return new CorsFilter();
+	}
 }
